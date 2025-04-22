60b07ecfc01bc7a2e53160b113c7d9c22f958b6e
vaqueni <sungsan1204@gmail.com>
2025-04-22 15:22:53 +0900

    ch3 innit

---
+++ ch3.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import sys
+import tempfile
+import os
+
+# pointRead.py 문자열 저장
+pointread_code = IN[0]
+
+temp_dir = tempfile.gettempdir()
+pointread_path = os.path.join(temp_dir, "pointRead.py")
+with open(pointread_path, "w", encoding="utf-8") as f:
+    f.write(pointread_code)
+
+# 저장된 경로 import
+if temp_dir not in sys.path:
+    sys.path.append(temp_dir)
+'''
+# 매번 새로 불러오기가 가능해야하지만 노드연결을 연결해제후 재연결해야만함.
+
+import importlib
+import pointRead
+importlib.reload(pointRead)  
+'''
+
+# pointRead에서 불러오기
+from pointRead import *
+
+# 파일 읽기
+csv_path = IN[1]
+df = pd.read_csv(csv_path, header=13, encoding='cp949')
+filtered_df = df[['측점', 'Northing', 'Easting', '표고']]
+
+
+
+rawPoints = PointArray()
+
+for _, row in filtered_df.iterrows():
+    rawPoints.add_point(
+        LinPoint(
+            lin=row['측점'],
+            x=row['Easting'],
+            y=row['Northing'],
+            z=row['표고']
+        )
+    )
+
+rawPoints.to_rel()
+start = IN[2]
+end = IN[3]
+
+linear_point = rawPoints.points_slide(start,end)
+
+linear_point = linear_point.to_dynamo_points()
+
+OUT = PolyCurve.ByPoints(linear_point)
